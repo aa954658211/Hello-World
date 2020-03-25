14c2a7d1b90d0f53cd38c253b2dbe1c272a4ab94
aa954658211 <954658211@qq.com>
2020-03-25 20:40:22 +0800

    金坷垃

---
+++ src/main/C.java
@@ -0,0 +1,8 @@
+package main;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("大萨达所防守打法");
+	}
+}
--- src/main/java/A.java
+++ src/main/java/A.java
@@ -6,6 +6,6 @@ public class A {
 
 
     public static void main(String[] args) {
-        System.out.println("sdfsdfsd");
+        System.out.println("第三方第三方士大夫");
     }
 }
